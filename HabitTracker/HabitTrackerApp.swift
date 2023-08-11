5a1c169b56909027fb1814fbd11d2b8fc873cf3c
IanGaplichnik <ian.gaplichnik@protonmail.ch>
2023-08-11 11:02:29 +0200

    Initial Commit

---
+++ HabitTracker/HabitTrackerApp.swift
@@ -0,0 +1,17 @@
+//
+//  HabitTrackerApp.swift
+//  HabitTracker
+//
+//  Created by MacBook on 11.8.2023.
+//
+
+import SwiftUI
+
+@main
+struct HabitTrackerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
